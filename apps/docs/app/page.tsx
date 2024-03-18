b0962140976f8002a8fe2a5016ddc558afe053b2
kochava123 <135976371+kochava123@users.noreply.github.com>
2024-03-18 06:45:32 +0000

    scoping

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@zenith/ui/card";
+import { Code } from "@zenith/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@zenith/ui/button";
 
 function Gradient({
   conic,
